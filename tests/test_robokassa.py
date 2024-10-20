319c0b81a437aeb71e245d7e114f6defd9d54791
Ben Puls <mail@benpuls.ru>
2024-10-20 16:17:07 +0500

    add: test of validators and robokassa

--- tests/test_robokassa.py
+++ tests/test_robokassa.py
@@ -1,6 +1,12 @@
-
+import pytest
 
 from robokassa import Robokassa, HashAlgorithm
+from robokassa.types import RobokassaParams
+
+from robokassa.asyncio import Robokassa as AsyncRobokassa
+
+
+pytest_plugins = ("pytest_asyncio",)
 
 
 class TestRobokassaClient:
@@ -19,3 +25,45 @@ class TestRobokassaClient:
     def test_link_generator(self) -> None:
         link = self.robokassa.generate_link_to_payment_page(out_sum=1, inv_id=0)
         assert link
+
+    def test_robokassa_params(self) -> None:
+        dict_params = RobokassaParams(
+            merchant_login="test_login",
+            out_sum=1,
+            description="Hello, World!",
+            additional_params={"shp_id": 1234, "shp_data": "My data"},
+        )
+        assert isinstance(dict_params.as_dict(), dict)
+
+
+class TestRobokassaAsyncio:
+    robokassa = AsyncRobokassa(
+        is_test=True,
+        merchant_login="rayspirit",
+        password1="VGX70HgoVTiw0R85blhs",
+        password2="qU2ZgTvfyG65g80yqyfo",
+        algorithm=HashAlgorithm.md5,
+    )
+
+    @pytest.mark.asyncio
+    async def test_link(self) -> None:
+        link = self.robokassa.payments.link.generate_by_script(
+            out_sum=1,
+            inv_id=0,
+        )
+        assert link
+
+        link1 = await self.robokassa.payments.link.create_by_invoice_id(
+            inv_id=0,
+            description="Hello world!",
+            out_sum=1000,
+        )
+        assert link1
+
+    @pytest.mark.asyncio
+    async def test_currency(self) -> None:
+        data = await self.robokassa.get_currencies(language="ru")
+        print("\n", data)
+
+        assert data
+        assert isinstance(data, dict)
